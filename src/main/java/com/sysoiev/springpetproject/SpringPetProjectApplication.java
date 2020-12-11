2af29e3f95fbb263ffec421be20ef7c1c21aca28
Bada1208 <sysoievbogdan@gmail.com>
2020-12-11 10:10:39 +0100

    Initial commit

---
+++ src/main/java/com/sysoiev/springpetproject/SpringPetProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sysoiev.springpetproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPetProjectApplication.class, args);
+	}
+
+}
